4ffe78604059a2ac282d232819020a096cd70957
Dan Burton <danburton.email@gmail.com>
2012-08-12 08:44:45 -0600

    Turned BasicPrelude into a full Prelude replacement, created CorePrelude as what BasicPrelude used to be

--- BasicPrelude.hs
+++ BasicPrelude.hs
@@ -1,174 +1,112 @@
-{-# LANGUAGE NoImplicitPrelude #-}
 module BasicPrelude
-    ( -- * Standard
-      -- ** Operators
-      (Prelude.$)
-    , (Prelude.&&)
-    , (Prelude.||)
-    , (Control.Category..)
-      -- ** Functions
-    , Prelude.not
-    , Prelude.otherwise
-    , Prelude.fst
-    , Prelude.snd
-    , Control.Category.id
-    , Prelude.maybe
-    , Prelude.either
-    , Prelude.flip
-    , Prelude.const
-    , Prelude.error
-    , Prelude.zip
-    , Prelude.unzip
-    , Prelude.zipWith
-    , Prelude.or
-    , Data.Text.IO.putStrLn
-    , Prelude.elem
-    , Prelude.odd
-    , Prelude.even
-    , Prelude.uncurry
-      -- ** Type classes
-    , Prelude.Ord (..)
-    , Prelude.Eq (..)
-    , Prelude.Enum (..)
-    , Prelude.Show
-    , Prelude.Functor (..)
-    , Prelude.Monad (..)
-    , (Control.Monad.=<<)
-      -- ** Numeric type classes
-    , Prelude.Num (..)
-    , Prelude.Real (..)
-    , Prelude.Integral (..)
-    , Prelude.Fractional (..)
-    , Prelude.Floating (..)
-    , Prelude.RealFrac (..)
-    , Prelude.RealFloat(..)
-      -- ** Data types
-    , Prelude.Maybe (..)
-    , Prelude.Ordering (..)
-    , Prelude.Bool (..)
-    , Prelude.Char
-    , Prelude.IO
-    , Prelude.Either (..)
-      -- * Re-exports
-      -- ** Packed reps
-    , ByteString
-    , LByteString
-    , Text
-    , LText
-      -- ** Containers
-    , Map
-    , HashMap
-    , Set
-    , HashSet
-    , Vector
-    , UVector
-    , Unbox
-    , Hashable
-      -- ** Numbers
-    , Word
-    , Word8
-    , Word32
-    , Word64
-    , Prelude.Int
-    , Int32
-    , Int64
-    , Prelude.Integer
-    , Prelude.Rational
-    , Prelude.Float
-    , Prelude.Double
-      -- ** Numeric functions
-    , (Prelude.^)
-    , (Prelude.^^)
-    , Prelude.subtract
-    , Prelude.fromIntegral
-    , Prelude.realToFrac
-      -- ** Monoids
-    , Monoid (..)
-    , empty
-    , concat
-    , (++)
-      -- ** Arrow
-    , Control.Arrow.first
-    , Control.Arrow.second
-    , (Control.Arrow.***)
-    , (Control.Arrow.&&&)
-      -- ** Maybe
-    , Data.Maybe.mapMaybe
-    , Data.Maybe.catMaybes
-    , Data.Maybe.fromMaybe
-      -- ** Either
-    , Data.Either.partitionEithers
-      -- ** Applicative
-    , Control.Applicative.Applicative (..)
-    , (Control.Applicative.<$>)
-      -- ** Monad
-    , (Control.Monad.>=>)
-      -- ** Transformers
-    , Control.Monad.Trans.Class.lift
-    , Control.Monad.IO.Class.MonadIO
-    , Control.Monad.IO.Class.liftIO
-      -- ** Exceptions
-    , Control.Exception.Exception (..)
-    , Control.Exception.SomeException
-    , Control.Exception.throwIO
-      -- ** Files
-    , F.FilePath
-    , (F.</>)
-    , (F.<.>)
-    , F.hasExtension
-    , F.basename
-    , F.filename
-      -- ** Print
-    , Prelude.print
-    ) where
+  ( module CorePrelude
+  , module Data.List
+  , module Prelude
+  , module Data.Text
+  , show
+  , read
+  , putStr
+  , getLine
+  , getContents
+  , readFile
+  , writeFile
+  , appendFile
+  , readIO
+  ) where
 
-import qualified Prelude
-import Prelude (Char, (.))
-
-import Data.Hashable (Hashable)
-import Data.Vector.Unboxed (Unbox)
-
-import Data.Monoid (Monoid (..))
-import qualified Control.Arrow
-import qualified Control.Applicative
-import qualified Control.Category
-import qualified Control.Monad
-import qualified Control.Exception
-
+import qualified Data.Text as T
+import qualified Data.Text.IO as T
+import qualified Data.Text.Lazy.IO as TL
+import qualified Prelude as P
 import qualified Filesystem.Path.CurrentOS as F
 
-import Data.Word (Word8, Word32, Word64, Word)
-import Data.Int (Int32, Int64)
+import CorePrelude
 
-import qualified Data.Text.IO
+import Data.List hiding
+  ( -- already in CorePrelude
+    (++)
+  , concat
+  
+    -- prefer Text versions instead
+  , lines
+  , words
+  , unlines
+  , unwords
+  , intercalate
+  )
 
-import qualified Data.Maybe
-import qualified Data.Either
+import Prelude
+  ( sequence
+  , sequence_
+  , mapM
+  , mapM_
+  , Bounded (..)
+  , seq
+  , curry
+  , until
+  , asTypeOf
+  , undefined
+  , ReadS
+  , ShowS
+  , Read (..)
+  , Show (showsPrec, showList)
+  , reads
+  , shows
+  , showChar
+  , showString
+  , showParen
+  , readParen
+  , lex
+  , IOError
+  , ioError
+  , userError
+  , catch
+  , putChar
+  , getChar
+  , readLn
+  , interact
+  )
 
-import qualified Control.Monad.Trans.Class
-import qualified Control.Monad.IO.Class
+import Data.Text
+  ( lines
+  , words
+  , unlines
+  , unwords
+  , intercalate
+  )
 
-import Data.ByteString (ByteString)
-import qualified Data.ByteString.Lazy
-import Data.Text (Text)
-import qualified Data.Text.Lazy
-import Data.Vector (Vector)
-import qualified Data.Vector.Unboxed
-import Data.Map (Map)
-import Data.Set (Set)
-import Data.HashMap.Strict (HashMap)
-import Data.HashSet (HashSet)
+-- not exported, only used for code here
+import System.IO
+  ( hSetBuffering
+  , stdin
+  , stdout
+  , BufferMode (NoBuffering)
+  )
 
-type LText = Data.Text.Lazy.Text
-type LByteString = Data.ByteString.Lazy.ByteString
-type UVector = Data.Vector.Unboxed.Vector
+show :: Show a => a -> Text
+show = T.pack . P.show
 
-empty :: Monoid w => w
-empty = mempty
+read :: Read a => Text -> a
+read = P.read . T.unpack
 
-concat :: Monoid w => [w] -> w
-concat = mconcat
+putStr :: Text -> IO ()
+putStr = T.putStr
 
-infixr 5  ++
-(++) :: Monoid w => w -> w -> w
-(++) = mappend
+getLine :: IO Text
+getLine = T.getLine
+
+getContents :: IO LText
+getContents = TL.getContents
+
+readFile :: FilePath -> IO Text
+readFile = T.readFile . F.encodeString
+
+writeFile :: FilePath -> Text -> IO ()
+writeFile = T.writeFile . F.encodeString
+
+appendFile :: FilePath -> Text -> IO ()
+appendFile = T.appendFile . F.encodeString
+
+readIO :: Read a => Text -> IO a
+readIO = P.readIO . T.unpack
+
---
+++ CorePrelude.hs
@@ -0,0 +1,174 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+module CorePrelude
+    ( -- * Standard
+      -- ** Operators
+      (Prelude.$)
+    , (Prelude.&&)
+    , (Prelude.||)
+    , (Control.Category..)
+      -- ** Functions
+    , Prelude.not
+    , Prelude.otherwise
+    , Prelude.fst
+    , Prelude.snd
+    , Control.Category.id
+    , Prelude.maybe
+    , Prelude.either
+    , Prelude.flip
+    , Prelude.const
+    , Prelude.error
+    , Prelude.zip
+    , Prelude.unzip
+    , Prelude.zipWith
+    , Prelude.or
+    , Data.Text.IO.putStrLn
+    , Prelude.elem
+    , Prelude.odd
+    , Prelude.even
+    , Prelude.uncurry
+      -- ** Type classes
+    , Prelude.Ord (..)
+    , Prelude.Eq (..)
+    , Prelude.Enum (..)
+    , Prelude.Show
+    , Prelude.Functor (..)
+    , Prelude.Monad (..)
+    , (Control.Monad.=<<)
+      -- ** Numeric type classes
+    , Prelude.Num (..)
+    , Prelude.Real (..)
+    , Prelude.Integral (..)
+    , Prelude.Fractional (..)
+    , Prelude.Floating (..)
+    , Prelude.RealFrac (..)
+    , Prelude.RealFloat(..)
+      -- ** Data types
+    , Prelude.Maybe (..)
+    , Prelude.Ordering (..)
+    , Prelude.Bool (..)
+    , Prelude.Char
+    , Prelude.IO
+    , Prelude.Either (..)
+      -- * Re-exports
+      -- ** Packed reps
+    , ByteString
+    , LByteString
+    , Text
+    , LText
+      -- ** Containers
+    , Map
+    , HashMap
+    , Set
+    , HashSet
+    , Vector
+    , UVector
+    , Unbox
+    , Hashable
+      -- ** Numbers
+    , Word
+    , Word8
+    , Word32
+    , Word64
+    , Prelude.Int
+    , Int32
+    , Int64
+    , Prelude.Integer
+    , Prelude.Rational
+    , Prelude.Float
+    , Prelude.Double
+      -- ** Numeric functions
+    , (Prelude.^)
+    , (Prelude.^^)
+    , Prelude.subtract
+    , Prelude.fromIntegral
+    , Prelude.realToFrac
+      -- ** Monoids
+    , Monoid (..)
+    , empty
+    , concat
+    , (++)
+      -- ** Arrow
+    , Control.Arrow.first
+    , Control.Arrow.second
+    , (Control.Arrow.***)
+    , (Control.Arrow.&&&)
+      -- ** Maybe
+    , Data.Maybe.mapMaybe
+    , Data.Maybe.catMaybes
+    , Data.Maybe.fromMaybe
+      -- ** Either
+    , Data.Either.partitionEithers
+      -- ** Applicative
+    , Control.Applicative.Applicative (..)
+    , (Control.Applicative.<$>)
+      -- ** Monad
+    , (Control.Monad.>=>)
+      -- ** Transformers
+    , Control.Monad.Trans.Class.lift
+    , Control.Monad.IO.Class.MonadIO
+    , Control.Monad.IO.Class.liftIO
+      -- ** Exceptions
+    , Control.Exception.Exception (..)
+    , Control.Exception.SomeException
+    , Control.Exception.throwIO
+      -- ** Files
+    , F.FilePath
+    , (F.</>)
+    , (F.<.>)
+    , F.hasExtension
+    , F.basename
+    , F.filename
+      -- ** Print
+    , Prelude.print
+    ) where
+
+import qualified Prelude
+import Prelude (Char, (.))
+
+import Data.Hashable (Hashable)
+import Data.Vector.Unboxed (Unbox)
+
+import Data.Monoid (Monoid (..))
+import qualified Control.Arrow
+import qualified Control.Applicative
+import qualified Control.Category
+import qualified Control.Monad
+import qualified Control.Exception
+
+import qualified Filesystem.Path.CurrentOS as F
+
+import Data.Word (Word8, Word32, Word64, Word)
+import Data.Int (Int32, Int64)
+
+import qualified Data.Text.IO
+
+import qualified Data.Maybe
+import qualified Data.Either
+
+import qualified Control.Monad.Trans.Class
+import qualified Control.Monad.IO.Class
+
+import Data.ByteString (ByteString)
+import qualified Data.ByteString.Lazy
+import Data.Text (Text)
+import qualified Data.Text.Lazy
+import Data.Vector (Vector)
+import qualified Data.Vector.Unboxed
+import Data.Map (Map)
+import Data.Set (Set)
+import Data.HashMap.Strict (HashMap)
+import Data.HashSet (HashSet)
+
+type LText = Data.Text.Lazy.Text
+type LByteString = Data.ByteString.Lazy.ByteString
+type UVector = Data.Vector.Unboxed.Vector
+
+empty :: Monoid w => w
+empty = mempty
+
+concat :: Monoid w => [w] -> w
+concat = mconcat
+
+infixr 5  ++
+(++) :: Monoid w => w -> w -> w
+(++) = mappend
