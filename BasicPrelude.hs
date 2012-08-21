7b1df9d599a89afb4aac40504c52a2b4062e13a5
Dan Burton <danburton.email@gmail.com>
2012-08-20 18:54:51 -0600

    Re-arranging BasicPrelude to better haddock its exports (#16)

--- BasicPrelude.hs
+++ BasicPrelude.hs
@@ -1,12 +1,19 @@
 {-# LANGUAGE NoImplicitPrelude #-}
 
+-- | BasicPrelude mostly re-exports
+-- several key libraries in their entirety.
+-- The exception is Data.List,
+-- where various functions are replaced
+-- by similar versions that are either
+-- generalized, operate on Text,
+-- or are implemented strictly.
 module BasicPrelude
-  ( module CorePrelude
+  ( -- * Module exports
+    module CorePrelude
   , module Data.List
-  , module Prelude
-  , module Data.Text
-  , module Data.Text.Lazy.IO
   , module Control.Monad
+
+    -- * Novel exports
   , map
   , show
   , read
@@ -22,13 +29,38 @@ module BasicPrelude
   , empty
   , (++)
   , concat
-  ) where
 
-import qualified Data.Text as T
-import qualified Data.Text.IO as T
-import qualified Data.Text.Lazy.IO as TL
-import qualified Prelude as P
-import qualified Filesystem.Path.CurrentOS as F
+    -- * Text exports
+  , Text.lines
+  , Text.words
+  , Text.unlines
+  , Text.unwords
+  , Text.intercalate
+  , LText.interact
+
+    -- * Prelude re-exports
+  , Prelude.Bounded (..)
+  , Prelude.gcd
+  , Prelude.lcm
+  , Prelude.String
+  , Prelude.ReadS
+  , Prelude.ShowS
+  , Prelude.Read (..)
+  , Prelude.Show (showsPrec, showList)
+  , Prelude.reads
+  , Prelude.shows
+  , Prelude.showChar
+  , Prelude.showString
+  , Prelude.showParen
+  , Prelude.readParen
+  , Prelude.lex
+  , Prelude.IOError
+  , Prelude.ioError
+  , Prelude.userError
+  , Prelude.putChar
+  , Prelude.getChar
+  , Prelude.readLn
+  ) where
 
 import CorePrelude
 
@@ -52,56 +84,27 @@ import Data.List hiding
   , product
   )
 
-import Prelude
-  ( Bounded (..)
-  , gcd
-  , lcm
-  , String
-  , ReadS
-  , ShowS
-  , Read (..)
-  , Show (showsPrec, showList)
-  , reads
-  , shows
-  , showChar
-  , showString
-  , showParen
-  , readParen
-  , lex
-  , IOError
-  , ioError
-  , userError
-  , putChar
-  , getChar
-  , readLn
-  )
-
-import Data.Text
-  ( lines
-  , words
-  , unlines
-  , unwords
-  , intercalate
-  )
-
-import Data.Text.Lazy.IO
-  ( interact
-  )
-
 -- Import *all of the things* from Control.Monad,
 -- specifically, the list-based things that
 -- CorePrelude doesn't export
 import Control.Monad
+
+import qualified Data.Text as Text
+import qualified Data.Text.IO as Text
+import qualified Data.Text.Lazy.IO as LText 
+import qualified Filesystem.Path.CurrentOS as FilePath
+import qualified Prelude
+
 
 
 map :: (Functor f) => (a -> b) -> f a -> f b
 map = fmap
 
 show :: Show a => a -> Text
-show = T.pack . P.show
+show = Text.pack . Prelude.show
 
 read :: Read a => Text -> a
-read = P.read . T.unpack
+read = Prelude.read . Text.unpack
 
 sum :: Num a => [a] -> a
 sum = foldl' (+) 0
@@ -110,25 +113,25 @@ product :: Num a => [a] -> a
 product = foldl' (*) 1
 
 putStr :: Text -> IO ()
-putStr = T.putStr
+putStr = Text.putStr
 
 getLine :: IO Text
-getLine = T.getLine
+getLine = Text.getLine
 
 getContents :: IO LText
-getContents = TL.getContents
+getContents = LText.getContents
 
 readFile :: FilePath -> IO Text
-readFile = T.readFile . F.encodeString
+readFile = Text.readFile . FilePath.encodeString
 
 writeFile :: FilePath -> Text -> IO ()
-writeFile = T.writeFile . F.encodeString
+writeFile = Text.writeFile . FilePath.encodeString
 
 appendFile :: FilePath -> Text -> IO ()
-appendFile = T.appendFile . F.encodeString
+appendFile = Text.appendFile . FilePath.encodeString
 
 readIO :: Read a => Text -> IO a
-readIO = P.readIO . T.unpack
+readIO = Prelude.readIO . Text.unpack
 
 
 empty :: Monoid w => w
@@ -140,3 +143,4 @@ infixr 5 ++
 
 concat :: Monoid w => [w] -> w
 concat = mconcat
+
